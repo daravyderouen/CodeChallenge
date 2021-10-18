98ac361a1358da24900b8c9f5a8dcac609a73e72
Daravy DeRouen <daravyderouen@gmail.com>
2021-10-17 19:31:15 -0500

    Revised to the correct array

--- Find_The_Range_Challenge.js
+++ Find_The_Range_Challenge.js
@@ -1,12 +1,12 @@
 
-const num = [1, 4, 11, 2, 37, -4];
+let array = [28, 43, -12, 30, 4, 0, -36];
 
-num.forEach(function(item) {
+array.forEach(function(item) {
     console.log(item);
 });
 
-console.log(Math.max(...num));
-console.log(Math.min(...num));
+console.log(Math.max(...array));
+console.log(Math.min(...array));
 
 
 
