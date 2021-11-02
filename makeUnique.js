81898935b5233086900639cb83efdbe1b3018aa0
Daravy DeRouen <daravyderouen@gmail.com>
2021-11-02 09:25:13 -0500

    Revised makeUnique challenge

---
+++ makeUnique.js
@@ -0,0 +1,16 @@
+// Write your solution below:
+//write a function to remove all duplicate letters from string(slice)
+//toLowercase is used
+//trim() is used
+
+
+let myString = "Daravy Inthavongsy DeRouen";
+
+myString = myString
+   .split("")
+   .filter((item, index, array) => array.indexOf(item) === index)
+   .join("")
+   .toLocaleLowerCase()
+   .replace(/\s/g, "");
+   
+console.log(myString); // "darvyinthogseru"
